029f3e51cf576ee2bbbc4b82b5020cf385afe983
shubhi2092 <104774342+shubhi2092@users.noreply.github.com>
2023-07-31 01:07:48 +0530

    commit 2

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react"
-import './home.scss'
+import './home.css'
 const Home = () => {
     return <div className="home">
         <div className="form">
@@ -33,97 +33,100 @@ const TooltipForm = () => {
 
     return (
         <>
+        <div className="screen">
+        <div className="tooltipform">
             <form>
-                <label>
-                    Target Element
-                    <select value={selectedButton} onChange={handleOptionChange}>
-                        <option value={0}>Button 1</option>
-                        <option value={1}>Button 2</option>
+               <div>
+                <label className="cascader">
+                    <div className="heading1">Target Element</div>
+                    <select className="inp1"value={selectedButton} onChange={handleOptionChange}>
+                        <option  value={0}>Button 1</option>
+                        <option  value={1}>Button 2</option>
                         <option value={2}>Button 3</option>
-                        <option value={3}>Button 4</option>
+                        <option  value={3}>Button 4</option>
                         <option value={4}>Button 5</option>
                     </select>
                 </label>
-
+                </div>
                 <br />
-
+                <div className="tooltiptext">
                 <label>
-                    Tooltip text
-                    <input type="text" value={tooltipText}
+                    <div className="heading1">Tooltip text</div>
+                    <input className="inp1" type="text" value={tooltipText}
                         onChange={(e) => {
                             settoolTipText(e.target.value)
                             buttons[selectedButton].current.innerHTML = e.target.value
                         }}
                     />
                 </label>
-
+                </div>
                 <br />
-
+               <div className="club1">
                 <label>
-                    Text Size
-                    <input type="number" value={size}
+                    <div className="heading1">Text Size </div>
+                    <input className="inp2" type="number" value={size}
                         onChange={(e) => {
                             setSize(e.target.value)
                             buttons[selectedButton].current.style.fontSize = e.target.value + "px"
                         }}
                     />
                 </label>
-
-                <br />
-
+                <div>
                 <label>
-                    Padding
-                    <input type="number" value={padding} onChange={(e) => {
+                <div className="heading2">Padding </div>
+                    <input className="inp2" type="number" value={padding} onChange={(e) => {
                         setPadding(e.target.value)
                         buttons[selectedButton].current.style.padding = e.target.value + "px"
                     }} />
                 </label>
+                </div>
+                </div>
 
                 <br />
 
                 <label>
-                    Text Color
-                    <input type="text" value={color} onChange={(e) => {
+                    <div  className="heading1">Text Color</div>
+                    <input className="inp1"type="text" value={color} onChange={(e) => {
                         setColor(e.target.value)
                         buttons[selectedButton].current.style.color = e.target.value
                     }} />
                 </label>
 
                 <br />
+                <br />
 
                 <label>
-                    Background color
-                    <input type="text" value={bgColor} onChange={(e) => {
+                    <div className="heading1">Background color</div>
+                    <input className="inp1" type="text" value={bgColor} onChange={(e) => {
                         setBg(e.target.value)
                         buttons[selectedButton].current.style.backgroundColor = e.target.value
                     }} />
                 </label>
 
                 <br />
-
+                <br />
+                <div className="club1">
                 <label>
-                    Corner radius
-                    <input type="number" value={borderRadius} onChange={(e) => {
+                    <div className="heading1">Corner radius</div>
+                    <input className="inp2" type="number" value={borderRadius} onChange={(e) => {
                         setBorderRadius(e.target.value)
                         buttons[selectedButton].current.style.borderRadius = e.target.value + "px"
                     }} />
                 </label>
 
-                <br />
-
                 <label>
-                    Tooltip width
-                    <input type="number" value={toolTipWidth} onChange={(e) => {
+                    <div class="heading2">Tooltip width</div>
+                    <input className="inp2" type="number" value={toolTipWidth} onChange={(e) => {
                         setTWidth(e.target.value)
                         buttons[selectedButton].current.style.width = e.target.value + "px"
                     }} />
                 </label>
-
+                </div>
                 <br />
-
+              <div class="club1">
                 <label>
-                    Arrow width
-                    <input type="number" value={arrowWidth} onChange={(e) => {
+                    <div class="heading1">Arrow width</div>
+                    <input className="inp2" type="number" value={arrowWidth} onChange={(e) => {
                         setAWidth(e.target.value)
                         const element = document.querySelectorAll('.tooltiptext')[selectedButton]
                         console.log(element)
@@ -132,41 +135,41 @@ const TooltipForm = () => {
                     }} />
                 </label>
 
-                <br />
-
                 <label>
-                    Arrow height
-                    <input type="number" value={arrowHeight} onChange={(e) => {
+                    <div class="heading2">Arrow height</div>
+                    <input className="inp2" type="number" value={arrowHeight} onChange={(e) => {
                         setAHeight(e.target.value)
                         const element = document.querySelectorAll('.tooltiptext')[selectedButton]
                         element.style.setProperty('--borderHeightAfter', e.target.value + "px")
                     }} />
                 </label>
-
+                </div>
                 <br />
             </form>
+            </div>
             <div className="renderer">
-                <button className="tooltip" onMouseEnter={(e) => {
-                    e.currentTarget.classList.add('tooltip-hover')
-                }}>Button 1
-                    <span className="tooltiptext" ref={buttons[0]}>Tooltip text</span>
+                <div className="mobilescreen">
+                <button id="topleft" className="tooltip">Button 1
+                    <span  className="tooltiptext" ref={buttons[0]}>Tooltip text</span>
                 </button>
-                <button className="tooltip"
+                <button id="topright" className="tooltip"
                 >Button 2
-                    <span className="tooltiptext" ref={buttons[1]}>Tooltip text</span>
+                    <span   className="tooltiptext" ref={buttons[1]}>Tooltip text</span>
                 </button>
-                <button className="tooltip"
+                <button  id="middle" className="tooltip"
                 >Button 3
-                    <span className="tooltiptext" ref={buttons[2]}>Tooltip text</span>
+                    <span  className="tooltiptext" ref={buttons[2]}>Tooltip text</span>
                 </button>
-                <button className="tooltip"
+                <button id="bottomleft" className="tooltip"
                 >Button 4
-                    <span className="tooltiptext" ref={buttons[3]}>Tooltip text</span>
+                    <span  className="tooltiptext" ref={buttons[3]}>Tooltip text</span>
                 </button>
-                <button className="tooltip"
+                <button id="bottomright" className="tooltip"
                 >Button 5
-                    <span className="tooltiptext" ref={buttons[4]}>Tooltip text</span>
+                    <span  className="tooltiptext" ref={buttons[4]}>Tooltip text</span>
                 </button>
+                </div>
+            </div>
             </div>
         </>
     );
